cbdd6a9d2e53e562e1a1a097bdcc4e740ad987de
SAYEM\mofak <sayemseer@gmail.com>
2025-03-06 22:06:34 -0600

    Struct

--- main.go
+++ main.go
@@ -1,116 +1,40 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const a = 10
-
-var p = 100
-
-func outer() func() {
-	money := 100
-	age := 30
-
-	fmt.Println("Age = ", age)
-
-	show := func() {
-		money = money + a + p
-		fmt.Println(money)
-	}
-	return show // CLOSURE
-}
-
-// A closure is a function that captures variables from its surrounding scope,
-// which can cause heap allocation.
-
-// Go তে escape analysis এর মাধ্যমে function (show) এবং
-// তার  parent function (outer) এর used variables (money) heap এ চলে যায়;
-// তারা সবাই মিলিতভাবে closure ক্রিয়েট করে।
-
-// Escape analysis is a way for the Go compiler to decide where to store variables
-// —either on the stack or on the heap.
-
-func call() {
-	incr1 := outer()
-
-	incr1()
-	incr1()
-
-	incr2 := outer()
-
-	incr2()
-	incr2()
+type User struct { // readonly - not possible to change user type later - code seg
+	Name string // member variable / property
+	Age  int
 }
 
 func main() {
-	call()
-}
+	var user1 User
 
-func init() {
-	fmt.Println("Bank")
+	user1 = User{ // user2 is instance or object of User type
+		Name: "Sayem",
+		Age:  30,
+	}
+
+	fmt.Println(user1.Name)
+
+	user2 := User{ // instantiate: process of creating instance
+		Name: "Roki",
+		Age:  60,
+	}
+
+	fmt.Println(user2.Age)
 }
 
 /*
+Code Segment - readonly
+------------
+User = type User struct {....}
+main = func() {....}
 
-1. Compilation Phase
-
-CODE Segment
----
-a
-outer = func(){...}
-outer:Anonymous1 = func(){...}
-call = func(){...}
-main = func(){...}
-init = func(){...}
-
-2. Execution Phase ->
-
-Code Segement will be copied to RAM code segment from file code segment(./main)
--------
-
-Data Seg
---------
-p
-
-Stack
-------
-stack frame create for init - print - pop from stack
+Data Segment
+------------
 stack frame for main
-stack frame for call
-
->>>>
-stack frame for outer (outer local variables will be here)
-    - money, age, show (referernce to outer:Anonymous1 in code segment)
-    - **money will go to heap (escape analysis) - so that we can acceess money when outer will be popped
-	- **show will go to heap (escape analysis)
-pop outer
-incr1 will be created on call stack
-	- show will be found on heap
-	- then found in code through reference
-stack frame for incr1 - money will be checked to local - data - code then will be found in heap
-                      - money will be accessed only by incr1
-					  - ** value of money will be changed
-pop incr1
-stack frame for incr1 - will get the new money value
-pop incr1
-
->>>>
-stack frame for outer
-    - new heap for money and show after doing escape analysis
-pop outer
-
-incr2 will be created on call stack
-stack frame for incr2
-pop incr2
-stack frame for incr2 - will get the new money value
-pop incr2
-
-pop call
+    - user1
+	- user2
 pop main
-
-Heap - GC
----------
-money
-show
 */
