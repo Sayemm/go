f10049524fa304629d2d67b06a601ca940fd46c8
SAYEM\mofak <sayemseer@gmail.com>
2025-03-21 12:30:48 -0600

    Pointer

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name   string
+	Age    int
+	Salary float32
+}
+
+// Pass by Value
+func printing(numbers [3]int) {
+	for _, number := range numbers {
+		fmt.Println(number)
+	}
+}
+
+// Pass by reference
 func print(numbers *[3]int) {
 	for _, number := range numbers {
 		fmt.Println(number)
@@ -9,28 +23,25 @@ func print(numbers *[3]int) {
 }
 
 func main() {
+	var address *int
+	num := 20
+	address = &num
+	fmt.Println(address)
+	fmt.Println(*address) // "dereferencing" or "indirecting"
+
 	arr := [3]int{1, 2, 3}
 	print(&arr)
+	printing(arr)
+
+	sayem_instance := User{
+		Name:   "Sayem",
+		Age:    24,
+		Salary: 33,
+	}
+
+	p := &sayem_instance
+
+	fmt.Println(sayem_instance.Age)
+	fmt.Println((*p).Name)
+	fmt.Println(p.Name) // https://go.dev/tour/moretypes/4
 }
-
-/*
-Code
-----
-print....
-main....
-
-data
-----
-
-stack
-------
-stack frame for array
-	array of size 3 - arr
-
-stack frame for print
-    only one space for numbers, which will point/refer the first address of the array
-	print array
-
-pop print
-pop main
-*/
