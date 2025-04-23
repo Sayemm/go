2dc66845ed8a1386a3bb3b25f34f6228159e3036
SAYEM\mofak <sayemseer@gmail.com>
2025-04-23 00:55:55 -0600

    Slice: Part of something (1)

--- main.go
+++ main.go
@@ -2,46 +2,36 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name   string
-	Age    int
-	Salary float32
-}
-
-// Pass by Value
-func printing(numbers [3]int) {
-	for _, number := range numbers {
-		fmt.Println(number)
-	}
-}
-
-// Pass by reference
-func print(numbers *[3]int) {
-	for _, number := range numbers {
-		fmt.Println(number)
-	}
-}
-
 func main() {
-	var address *int
-	num := 20
-	address = &num
-	fmt.Println(address)
-	fmt.Println(*address) // "dereferencing" or "indirecting"
+	arr := [6]string{"THIS", "IS", "A", "GO", "INTERVIEW", "QUESTION"}
 
-	arr := [3]int{1, 2, 3}
-	print(&arr)
-	printing(arr)
-
-	sayem_instance := User{
-		Name:   "Sayem",
-		Age:    24,
-		Salary: 33,
-	}
-
-	p := &sayem_instance
-
-	fmt.Println(sayem_instance.Age)
-	fmt.Println((*p).Name)
-	fmt.Println(p.Name) // https://go.dev/tour/moretypes/4
+	fmt.Println(arr)
 }
+
+/*
+2 Phase
+-------
+1. Compilation Phase - executible binary file create
+========================
+
+code segment
+---
+main().....
+
+2. Execution Phase
+========================
+- load the code segment to RAM
+code segment
+---
+main().....
+
+data segment
+---
+
+stact
+----
+stack frame for main
+   -- arr
+   -- print
+pop main
+*/
