b7cba5674c71c42dd3abc728938e30791bb4a506
SAYEM\mofak <sayemseer@gmail.com>
2025-05-02 22:51:17 -0600

    Slice Maintains 3 things: Pointer, Length, Capacity (2)

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ func main() {
 	arr := [6]string{"THIS", "IS", "A", "GO", "INTERVIEW", "QUESTION"}
 
 	fmt.Println(arr)
+
+	s := arr[1:4] //points arr ["IS", 3, 5]
+	fmt.Println(s)
+
+	s1 := s[1:2] // untimately points arr as well ["A", 1, 4]
+	fmt.Println(s1)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
 }
+
+// Slice Maintains 3 things: Pointer(1), Length(3), Capacity(5 -> starts at 1 to end)
 
 /*
 2 Phase
@@ -32,6 +42,8 @@ stact
 ----
 stack frame for main
    -- arr
+   ** s (ptr, len, cap)
+   ** s1 (ptr, len, cap)
    -- print
 pop main
 */
