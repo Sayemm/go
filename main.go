5f6c42362e10c0ae7292137232595506d2ce737e
Sayemm <sayemseer@gmail.com>
2025-06-02 00:24:29 -0600

    Context Switching | PCB | Concurrency

--- main.go
+++ main.go
@@ -18,80 +18,36 @@ func main() {
 }
 
 /*
-Computer 8 bits - means register set 8 bits - means - 000000000
-Ram and HD will be 8 bits/1 byte as well
-RAM - 0(Each will hold 8 bits) 1 2 3 4 5 6 7 8 .....
+Context Switching | PCB | Concurrency
+-------------------------------------
 
-Computer 16 bits / 2 bytes
-RAM - each cell will allocate 16 bits / 2 bytes (8 + 8)
-    - 0 2 4 6 8 10
+- Computer ON
+- .....
+- CU will start to execute instructions
 
-Computer 32 bits / 4 bytes
-RAM - each cell allocate 32 bits / 4 bytes (8 + 8 + 8 + 8)
-    - 0 4 8 12 16 20...
+- Software1, Software2, Software3  in RAM
+- Double click (Start) - loads to RAM 1, 2, 3 - 3 process
+- PC will point only one cell???!!! THEN HOW ARE 3 PROCESS RUNNING AT THE SAME TIME???
 
-Computer 64 bits / 8 bytes
-RAM - each cell allocate 64 bits / 8 bytes (8 x 8)
-    - 0 8 16...
+- MODERN COMPUTER CAN EXECUTE 10^8 INSTRUCTIONS IN A SEC (approx..)
+- Old OS ran 3 process one by one, no profit in that..
+- OS has a block name PCB (Process control block)
 
-- Computer ON (32 bits -> 0 4 8 12 16 20...)
-- OS will load to RAM from HD
-- Control Unit will tell PC to point the first address (0)
-- CU will fetch the instruction from the address that PC is indicating and put it to IR
-- CU read IR and decode the 32 bit instruction (+ 3 5)
-- EX: 3 will be on Accumulator Register (EAX), 5 on base register (EBX) and + on counter register (ECX)
-- EX: CU instruct ALU to add and CU will put it to data register (DAX)
-- PC value will increase and next instruction will be executed...
-- Now computer is ON and OS is running..
+- Software1 (28...)-100 instructions, Software2 (52...) 200 instructions, Software3 (72...)300 instructions
+- PC will be on 28
+- OS will instruct to execute let's say 10 instructions on Software1
+- Then OS will take PC to Software2 and PC will be 52
+- OS will instruct to execute let's say 20 instructions on Software2
+- Then OS will take PC to Software3 and PC will be 72
+- OS will instruct to execute let's say 20 instructions on Software3
+- .........
+- THIS IS CALLED CONTEXT SWITCHING
 
-- main.go will be saved to HD
-- compile the code (go build main.go)
-- binary file will be saved to HD
-- Execute (./main)
-	- OS will take the control
-	- OS will create a PROCESS (new memory will be allocated)
-		- CODE SEGMENT
-			- func main()..
-			- func add()...
-		- DATA SEGMENT
-		- STACK SEGMENT/FUNCTION FRAME - WILL CREATE FROM THE END
-			- stack frame for main..
-				- return address    ---80
-				- old BP value (8) and BP value will change ---76
-				- (a = 10) ----72
-				-** SP will be 72 and BP will be 76
-				==
-				-** Now SP = 72 and BP = 76 AGAIN
-				- sum = 14 ---68 -> S-> SP will be 68
-				- print will find sum using BP - 8 = 68
-				- pop sum -> SP will be 72
-				- pop a   -> SP will be 76
-				- SP = BP, stack is complete almost
-				- BP will be old value = 8
-				- return address means will go back to OS
-			== stack frame for add...
-				- y value = 4 ---68    -> SP will be 68
-				- x value = 1- --64    -> SP will be 64
-				- return address -- 60 -> SP will be 60
-				- old BP value (76) and BP value will change ---56 -> SP will be 56
-				- result = 0 -- 52 > SP will be 52
-					- Computer will take help from BP to find X (BP + 8) and Y (BP + 12) and Result (BP - 4)
-					- result = summation of X, Y
-				- return result
-					- pop 52 and SP will be 56
-					- add stack pointer will return result to main stack pointer
-					- BP will be now 76 and pop stack pointer
-					- SP will be 72
-			-** Now SP = 72 and BP = 76
-		- HEAP
-	- OS will give the CPU control to process
-	- PC will point the first cell of the process
-
-
-=> SP: When stack will change the SP value will change (increase/decrease)
-=> BP:
-	This is fixed in a certain stack frame,
-	using BP we can access variables in a stack frame (+/-)
-	when stack got popped BP will be the old BP value.
-
+- HOW PC knows where to go back of software1/2/3 to execute the rest instructions and what about past registers values?
+- PCB comes to play here, IT TRACKS ALL THE PROCESS
+- State (all registers valueS (SP, BP, IR, PC, AL, BL, CL, DL...), PID (process ID), ....)
+- This state is saved to PCB and goes to 2nd process and check for process 2 in PCB.....
+- OUR BRAIN THINKS EVERYTHING IS HAPPENNING AT THE SAME TIME (1/10) BUT NOT..
+	- THIS IS CONCURRENCY - EVERYTHING IS NOT HAPPENNING AT THE SAME TIME, HAPPENNING BY SWITCHING CONTEXT, HANDLING LOTS OF PROCESS AT THE SAME TIME
+	- IT GIVES A FEEL THAT, EVERYTHING IS HAPPENNING AT THE SAME TIME BUT NOT
 */
