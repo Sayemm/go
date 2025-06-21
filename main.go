2392d3ea4a0a49ffe68abfc8df47af28c7d7ce15
Sayemm <sayemseer@gmail.com>
2025-06-21 00:57:16 -0600

    defer list pointer

--- main.go
+++ main.go
@@ -13,31 +13,22 @@ func calculate() (result int) {
 	defer show()
 
 	result = 5
-	fmt.Println("Second: ", result) // 5
-	return
-}
 
-func calc() int {
-	result := 0
-	fmt.Println("First: ", result) // 0
-
-	show := func() {
-		result += 10
-		fmt.Println("Defer: ", result) // 15
+	p := func(a int) {
+		fmt.Println(a)
 	}
-	defer show()
+	defer p(result)
 
-	result = 5
+	defer fmt.Println(result)       // 5
 	fmt.Println("Second: ", result) // 5
-	return result
+	defer fmt.Println(5)
+
+	return
 }
 
 func main() {
 	res1 := calculate() // 15
 	fmt.Println(res1)
-
-	res := calc() // 5
-	fmt.Println(res)
 }
 
 /*
@@ -48,8 +39,7 @@ code segment
 ------------
 calculate := func..
 CalculateAnonymous.1 := func..
-calc := func...
-calcAnonymous.1 := func...
+CalculateAnonymous.2 := func..
 main := func...
 
 data segment
@@ -61,50 +51,17 @@ stack
 - stack frame for calculate
 	- result = 0
 	- print result = 0
-	- show variable = CalculateAnonymous.1 reference from code segment
-	- defer show() => store this CalculateAnonymous.1 somewhere AS A CLOSURE*********
-		-> (form closure show(), copy of result = 0?!!)
-		-> it saves the address/pointer of result variable because parent and closure will exist in stack frame
+	- show = CalculateAnonymous.1 reference from code segment
+	- another cell for defer => defer list pointer => some address
+	* defer show() => store this CalculateAnonymous.1, result somewhere (maybe HEAP) AS A CLOSURE*********
 	- result = 5
-	- print result = 5
-	- before returning will evaluate CalculateAnonymous.1
-	- stack frame for CalculateAnonymous.1
-		- copy of result = 0 + 10 = 10 ?! NO? because calcualte stack frame is still there, not popped and no value the reference of result variable is there
-		- calculate exists, clousre exists and closure function is evaluating.. in this case it won't take the copy of result
-		- result is the pointer of calcualte result
-		- result will be 5 + 10 = 15 in calculate stack frame
-	- pop CalculateAnonymous.1
-	- return 15
+	- p = CalculateAnonymous.2 reference from code segment
+	* CalculateAnonymous.2 will be stored in heap with argument as result = 5
+	* Println will be storead in heap with argument = 5
+	- print second 5
+	* Println will be storead in heap with argument = 5
+
+	- defer functions will be exeuted using defer list pointer..
+	- and defer list pointer will be updated from the next pointer until it becomes nil
 - pop calculate
-
-- stack frame for calc
-	- result = 0
-	- print result = 0
-	- show variable = CalcAnonymous.1 reference from code segment
-	- defer show() => store this CalcAnonymous.1 somewhere AS A CLOSURE (show(), result <-pointer)*********
-	- result = 5
-	- print result = 5
-	==> result value will be evaluated as return (this value will be returned and stored, so does not matter if this value changes somewhere)
-	- before returning will evaluate CalcAnonymous.1
-	- stack frame for CalcAnonymous.1
-		- result value will be updated (pointer -> 5) + 10 = 15
-		- print 15
-	- pop CalceAnonymous.1
-	- return 5 as this was evaluted earlier and stored somewhere
-- pop calc
-- pop main
-
-RULES
------
-NAMED RETURN VALUE
-1. all code exexute
-1. defer function will be stored on a magic box
-3. return -> all defer functions will be executed
-4. return the value of named return values
-
-JUST RETURN TYPE
-1. all code exexute
-1. defer function will be stored on a magic box
-3. return values will be evalued/stored at this time before defer (if defer even change the value does not matter)
-4. all defer functions will be executed
 */
