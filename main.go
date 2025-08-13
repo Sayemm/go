14bf743319d9b71b5bb82d09a8b79f1d5d7a76ea
Sayemm <sayemseer@gmail.com>
2025-08-12 22:34:11 -0600

    go runtime - simulation

--- main.go
+++ main.go
@@ -121,4 +121,44 @@ GO SCHEDULING
 		- if logical processor does not have anything to do (on his own local run queue and others' run queue) then it will go to the global run queue
 
 
+=========================
+- ./main
+- main go routune will be created that will run go runtime
+- now go runtime will create another thread (epoll_wait) and another thread for GC
+- go scheduler maintains m:p:g and creates m OS Thread that will run go virtual processor
+
+- go runtime now will run all init function
+- then creates another goroutine (main [main.go] goroutine) that runs on heap (on the stack main thread is running go runtime)
+	- main go routine will go to the local run queue of one of the virtual processor
+	- stack for that main go routine and main stack frame create
+		- mux variable on main function
+		- NewServeMux stack frame
+		- serve stack frame (there is an infinite loop in there)
+			- Accept stack frame
+				- goroutine tells go runtime to create a socket
+				- go runtime tells (sys call - epoll_ctl/netpoll) Kernal to create a socket, before go runtime will also check whether the socket is already created or not.
+				- go runtimes just tell the Kernal to create the socket and leave (go runtime keeps doing it's work)
+				- go runtime then sends that go routine to SLEEP
+				- socket is line a pipe (both way)
+				- Kernal creates a FD and corresponding socket and give it an endpoint (8080)
+
+				- go routine AWAKE - request to that URL (localhost:8080)
+				- router receives the request then request goes to computer/servers NIC
+				- NIC writes this in the Receice Buffer (on the RAM) then interrupt request to kernal
+				- Kernal reads from NIC receive bufffer and writes this in that socket receive buffer AND marks the FD of that socket
+				- then Kernal awake the 2nd thread (epoll_wait) and send that FD - epoll_wait thread send that FD to go runtime
+				- go runtime finds out who are sleeping for that particular FD and awake that go routine and give that FD
+				- now that sleeping goroutine started to run again by a virtual processor
+				- now that go routine reads DATA from the socket buffer that is mapped by that FD (rw, err := l.Accept()) and keep it on rw
+				- now that man go routine created another goroutine (go c.serve(connCtx)) and give it the responsibility
+					- new stack will be allocated for new go routine
+					- new goroutine will find the path /about and finds out which handlers/route are being registered in the router and match
+					- and write data to socket send buffer
+					- Kernal writes this data to NIC ring buffer
+					- NIC send data to router and finally sender will see the data
+				- main go routine will be ready again and call l.Accept().., tells to socket again but go runtime will find that socket is already created
+				- it will just wait for kernal to mark the socket buffer
+
+
+
 */
