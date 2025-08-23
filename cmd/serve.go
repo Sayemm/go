cef89080bef1f302de4a09ed60158b5f67ee22b7
Sayemm <sayemseer@gmail.com>
2025-08-23 12:13:02 -0600

    middleware - refactoring

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,25 +5,16 @@ import (
 	"ecommerce/handlers"
 	"ecommerce/middleware"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func Serve() {
 	mux := http.NewServeMux() // mux = router
 
-	//*********
-	controller := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("I am handler")
-	}
-	handler := http.HandlerFunc(controller)
-	// mux.Handle("GET /route", handler)
-	mux.Handle("GET /route", middleware.Logger(handler))
-	//*********
-
-	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProducts))
-	mux.Handle("POST /products", http.HandlerFunc(handlers.CreateProduct)) // need to write the resouce name (REST) // route/entity will be plural
-	mux.Handle("GET /products/{productId}", http.HandlerFunc(handlers.GetProductById))
+	mux.Handle("GET /route", middleware.Logger(http.HandlerFunc(handlers.Test)))
+	mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handlers.GetProducts)))
+	mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handlers.CreateProduct))) // need to write the resouce name (REST) // route/entity will be plural
+	mux.Handle("GET /products/{productId}", middleware.Logger(http.HandlerFunc(handlers.GetProductById)))
 
 	globalRouter := global_router.GlobalRouter(mux)
 
