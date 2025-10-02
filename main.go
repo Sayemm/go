3db45e4fef4854601105120bbf9f881a68488f11
Sayemm <sayemseer@gmail.com>
2025-10-02 14:30:13 -0600

    E-commerce Project - Create & Login User

--- main.go
+++ main.go
@@ -9,13 +9,7 @@ func main() {
 }
 
 /*
-Middleware
-==========
-- GET /products -> getProducts (this function is doing handleCors and handlePreflight!! - NOT SOLID)
-- GET /products -> (handleCors -> handlePreflight) Middleware -> getProducts (handler/controller)
-
--> GlobalRouter - Route Match - Controller
--> anything before controller is middleware
--> if we do anything after controller than controller will become middleware (no restrictions)
+JWT (JSON Web Token) - Authentication
+======================================
 
 */
