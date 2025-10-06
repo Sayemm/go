53e28f5d1e89ecbafa9dd3e7064806663e236f3f
Sayemm <sayemseer@gmail.com>
2025-10-06 10:29:38 -0600

    Interface - struct and interface type become same

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
-import (
-	"ecommerce/cmd"
-)
+import "fmt"
+
+// import (
+// 	"ecommerce/cmd"
+// )
+
+// Only Signature of the function
+type People interface {
+	PrintDetails()
+}
+
+type user struct {
+	Name  string
+	Age   int
+	Money float64
+}
+
+// receiver function / method / behavior
+func (obj user) PrintDetails() {
+	fmt.Println(obj.Name)
+}
 
 func main() {
-	cmd.Serve()
+	var u1 People
+
+	u1 = user{ // instantiation - object/instance creation
+		Name:  "Sayem",
+		Age:   26,
+		Money: 23,
+	}
+	u1.PrintDetails()
 }
+
+/*
+Singleton Design Pattern
+------------------------
+- everyone is sharing ONE thing/object
+- We are loding config only one time using pointer
+
+
+Paradigms
+---------
+- Structured Programming Language
+- Object Oriented Programming Language
+- Functional Programming Language
+	- Pure Functional Paradigm - same input same output always
+
+Interface
+=========
+- Abstractive - Concept (dharona)
+- Interface - Pure Abstraction, No details
+
+Struct
+======
+- struct can implement interface
+- struct and interface er type same hoe jay if the struct has a same receiver function
+*/
