6a7d545e80e7155cdbd68595b3b26ce559cff17c
Sayemm <sayemseer@gmail.com>
2025-10-09 09:57:09 -0600

    database - CRUD

---
+++ db_queries/0012-Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    password
+) VALUES (
+    'Seer',
+    'Sayem',
+    'ss@gmail.com',
+    '1234'
+);
---
+++ db_queries/0013-Update.sql
@@ -0,0 +1,4 @@
+UPDATE users SET
+    first_name = 'Habibur',
+    last_name = 'Rahman'
+WHERE id = 5;
