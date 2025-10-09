505801eb2b47045a7c9699ffaa3f7ada50271b71
Sayemm <sayemseer@gmail.com>
2025-10-09 15:02:25 -0600

    database - migrations

--- main.go
+++ main.go
@@ -22,4 +22,10 @@ db
 - application connects with db and app gets a client
 - client will store/delete... data from db
 - database connection - library sqlx/sqlc/ORM (gorm)
+
+db migration
+------------
+- what if we need to change column in a table?
+- we don't do it manually
+- migration comes into play in this scenario
 */
