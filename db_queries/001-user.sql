622e796f689e42095e00757fa8b2348370c89a2b
Sayemm <sayemseer@gmail.com>
2025-10-09 09:18:46 -0600

    database - data types

--- db_queries/001-user.sql
+++ db_queries/001-user.sql
@@ -7,3 +7,28 @@ CREATE TABLE users (
     is_shop_owner BOOLEAN DEFAULT FALSE
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+/*
+DB Data Types
+-------------
+SERIAL - 1, 2, 3, 4, ..... (32 bits)
+BIGSERIAL - (64 bits)
+
+SMALLINT - 16 bits
+INT - 32 bits
+BIGINT - 64 bits
+
+REAL - 32 bits - 10.32
+DOUBLE PRECISION - 64 bits
+
+CHAR(n) - Fixed upto n - CHAR(6) => 'a      ' (5xspace)
+VARCHAR(n) - Not Fixed upto n
+TEXT  -> unlimited
+
+BOOLEAN -> true/false
+
+TIME - without time zone -> Time of the day (9:32)
+DATE - 2025-09-09
+TIMESTAMP - Without time zone - 2025-09-09 9:32.43
+TIMESTAMP WITH TIME ZONE - 2025-09-09 9:32.43+6
+*/
