cb5deaf401c7328e00a5d64cd284b7c8170ac512
Sayemm <sayemseer@gmail.com>
2026-01-14 17:40:59 -0600

    Docker setup working with database

--- ecommerce-api/cmd/serve.go
+++ ecommerce-api/cmd/serve.go
@@ -3,46 +3,75 @@ package cmd
 import (
 	"ecommerce/config"
 	"ecommerce/infra/db"
-	"ecommerce/product"
-	"ecommerce/repo"
-	"ecommerce/rest"
-	productHandler "ecommerce/rest/handlers/product"
-	userHandler "ecommerce/rest/handlers/user"
-	"ecommerce/rest/middleware"
-	"ecommerce/user"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func Serve() {
+	fmt.Println("Step 1: Loading configuration...")
 	cnf := config.GetConfig()
 
+	fmt.Println("Step 2: Connecting to database...")
 	dbCon, err := db.NewConnection(cnf.DB)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer dbCon.Close()
 
+	fmt.Println("Step 3: Running database migrations...")
 	err = db.MigrateDB(dbCon, "./migrations")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	middlewares := middleware.NewMiddlewares(cnf)
+	fmt.Println("Step 4: Setting up routes...")
+	mux := http.NewServeMux()
 
-	// repo
-	userRepo := repo.NewUserRepo(dbCon)
-	productRepo := repo.NewProductRepo(dbCon)
+	// Health check endpoint (to verify server is running)
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `{"status":"healthy","service":"%s","version":"%s"}`,
+			cnf.ServiceName, cnf.Version)
+	})
 
-	// domains
-	userSvc := user.NewService(userRepo)
-	productSvc := product.NewService(productRepo)
+	// Root endpoint (welcome message)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `{"message":"Welcome to %s API","version":"%s"}`,
+			cnf.ServiceName, cnf.Version)
+	})
 
-	// handlers
-	productHandler := productHandler.NewHandler(middlewares, productSvc)
-	userHandler := userHandler.NewHandler(cnf, userSvc)
+	// STEP 5: Start HTTP Server
+	address := fmt.Sprintf(":%d", cnf.HttpPort)
 
-	server := rest.NewServer(cnf, productHandler, userHandler)
-	server.Start()
+	fmt.Printf("Server running on port %d\n", cnf.HttpPort)
+
+	err = http.ListenAndServe(address, mux)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v\n", err)
+		os.Exit(1)
+	}
+
+	// middlewares := middleware.NewMiddlewares(cnf)
+
+	// // repo
+	// userRepo := repo.NewUserRepo(dbCon)
+	// productRepo := repo.NewProductRepo(dbCon)
+
+	// // domains
+	// userSvc := user.NewService(userRepo)
+	// productSvc := product.NewService(productRepo)
+
+	// // handlers
+	// productHandler := productHandler.NewHandler(middlewares, productSvc)
+	// userHandler := userHandler.NewHandler(cnf, userSvc)
+
+	// server := rest.NewServer(cnf, productHandler, userHandler)
+	// server.Start()
 }
